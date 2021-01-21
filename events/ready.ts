76c904319d58eaaf23243ef4b33fcd51c0e23527
ZiomaleQ <r.partyka30@gmail.com>
2021-01-21 22:35:08 +0100

    Adding ready event

---
+++ events/ready.ts
@@ -0,0 +1,8 @@
+import { Client, User } from "../deps.ts";
+
+export default {
+  name: "READY",
+  run: (client: Client, user: User) => {
+    console.log("Logged as " + user.data.username);
+  },
+};
