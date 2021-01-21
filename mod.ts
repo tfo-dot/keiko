16062e6ffeb52ba599a38893c7dd58be0099a359
ZiomaleQ <r.partyka30@gmail.com>
2021-01-21 22:40:22 +0100

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,9 +1,10 @@
 import { Client } from "./deps.ts";
+import { serve } from "https://deno.land/std@0.84.0/http/server.ts";
 
 let debug = [...Deno.readDirSync("./")].find((entry) =>
   entry.name == "token.ts"
 );
-let Keiko = new Client({ hotreload: true, prefix: "=", debug: !!debug });
+let Keiko = new Client({ hotreload: true, prefix: "keiko!", debug: !!debug });
 
 let token = debug ? (await import(`./token.ts`)).token : Deno.env.get("TOKEN");
 
@@ -14,10 +15,8 @@ setInterval(
   5 * 60 * 1000,
 );
 
-import { serve } from "https://deno.land/std@0.84.0/http/server.ts";
-
-const server = serve({ hostname: "0.0.0.0", port: 8080 });
-console.log(`HTTP webserver running.  Access it at:  http://localhost:8080/`);
+const server = serve({ hostname: "0.0.0.0", port: 80 });
+console.log(`HTTP webserver running.  Access it at:  http://localhost:80/`);
 
 for await (const request of server) {
   request.respond({ status: 200, body: "Bonjour!" });
