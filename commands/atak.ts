44243b533bd5cff2a909d84d45fc4d0301b895ff
ZiomaleQ <r.partyka30@gmail.com>
2021-03-31 13:37:35 +0200

    atak slash command

--- commands/atak.ts
+++ commands/atak.ts
@@ -9,21 +9,23 @@ export default {
     "Użycie:",
     "`keiko!atak <poziom> [modyfikator] [dodatkowe obrażenia] [krytyczne] [wartość krytycznego] [drugi atak]`",
   ).field("Ogólny opis", "Licze obrażenia ataku podstawowego"),
-  run: (client: Client, msg: CommandisMessage) => {
-    if (msg.guild && msg.guild.id != "749007879150895105") {
-      return msg.reply(
-        `<@${msg.author.id}>, sorka ale coś poszło nie tak, szczegóły: \`Komenda nie jest wykonywana na serwerze SAO:Reborn\``,
+  run: (client: Client, msg: CommandisMessage | SlashCommandAtak) => {
+
+    if (!(msg as CommandisMessage).data) { }
+    if ((msg as CommandisMessage).guild && (msg as CommandisMessage).guild?.id != "749007879150895105") {
+      return (msg as CommandisMessage).reply(
+        `<@${(msg as CommandisMessage).author.id}>, sorka ale coś poszło nie tak, szczegóły: \`Komenda nie jest wykonywana na serwerze SAO:Reborn\``,
       );
     }
-    const ulvl = msg.stringReader.readInt()
+    const ulvl = !(msg as CommandisMessage).stringReader ? msg.lvl : ((msg as CommandisMessage).stringReader.readInt())
     const lvl = ulvl <= 0 ? 1 : ulvl;
-    const okay = genRandom(0, 40) + msg.stringReader.readInt();
-    const aDmg = msg.stringReader.readInt()
+    const okay = genRandom(0, 40) + (!(msg as CommandisMessage).stringReader ? msg.okayModif : ((msg as CommandisMessage).stringReader.readInt()))
+    const aDmg = !(msg as CommandisMessage).stringReader ? msg.addDmg : ((msg as CommandisMessage).stringReader.readInt())
     let dmg = genRandom(0, lvl * 15) + lvl * 15 + aDmg;
 
-    const crit = msg.stringReader.readInt();
-    const critVal = msg.stringReader.readInt();
-    const second = msg.stringReader.readInt();
+    const crit = !(msg as CommandisMessage).stringReader ? msg.crit : ((msg as CommandisMessage).stringReader.readInt())
+    const critVal = !(msg as CommandisMessage).stringReader ? msg.critMulti : ((msg as CommandisMessage).stringReader.readInt())
+    const second = !(msg as CommandisMessage).stringReader ? msg.second : ((msg as CommandisMessage).stringReader.readInt())
 
     const goCrit = genRandom(0, 100) > crit && crit > 0 || crit == 100;
     const goSecond = genRandom(0, 100) > second && second > 0 || second == 100;
@@ -31,22 +33,33 @@ export default {
     if (goCrit) dmg *= critVal <= 0 ? 2 : critVal / 100;
     if (goSecond) dmg *= 2;
 
-    const embed = new EmbedBuilder().title("No siemka");
+    let embed = new EmbedBuilder().title("No siemka");
     if (okay >= 15) {
-      return msg.reply(
-        new EmbedBuilder().title("No siemka").field(
-          "Informacje:",
-          `[${okay}] Trafiłeś ${goCrit ? "krytycznie" : ""} ${goSecond ? "podwójnie" : ""}, zadałeś ${dmg} obrażeń.`,
-        ).color("#00ff00"),
-      );
+      embed = embed.field(
+        "Informacje:",
+        `[${okay}] Trafiłeś${goCrit ? " krytycznie" : ""}${goSecond ? " podwójnie" : ""}, zadałeś ${dmg} obrażeń.`,
+      ).color("#00ff00")
     } else {
-      return msg.reply(
-        new EmbedBuilder().title("No siemka").field(
-          "Informacje:",
-          `[${okay}] Niestety, atak się nie udał...`,
-        )
-          .color("#ff0000"),
-      );
+      embed = embed.field(
+        "Informacje:",
+        `[${okay}] Niestety, atak się nie udał...`,
+      )
+        .color("#ff0000")
     }
-  },
+
+    if ((msg as CommandisMessage).data) {
+      (msg as CommandisMessage).reply(embed)
+    }
+
+    return embed
+  }
 };
+
+export interface SlashCommandAtak {
+  lvl: number,
+  okayModif: number,
+  addDmg: number,
+  crit: number,
+  critMulti: number,
+  second: number
+}
---
+++ events/interaction.ts
@@ -0,0 +1,28 @@
+import { Client, Interaction, EmbedBuilder } from "../deps.ts";
+
+export default {
+    name: "INTERACTION_CREATE",
+    run: async (client: Client, interaction: Interaction) => {
+        if (interaction.data.data?.name == "attack") {
+
+            let data = interaction.data.data.options ?? [];
+
+            let interactionData = {
+                lvl: (~~JSON.stringify(data.find(elt => elt.name == "lvl")?.value)) ?? 0,
+                okayModif: (~~JSON.stringify(data.find(elt => elt.name == "modyfikator")?.value)) ?? 0,
+                addDmg: (~~JSON.stringify(data.find(elt => elt.name == "obrażenia")?.value)) ?? 0,
+                crit: (~~JSON.stringify(data.find(elt => elt.name == "krytyczne")?.value)) ?? 0,
+                critMulti: (~~JSON.stringify(data.find(elt => elt.name == "mnożnik")?.value)) ?? 0,
+                second: (~~JSON.stringify(data.find(elt => elt.name == "second")?.value)) ?? 0
+            }
+
+            let response = (await ((await import("../commands/atak.ts")).default.run(client, interactionData))) as EmbedBuilder
+
+            interaction.reply({
+                embeds: [response.end()],
+                content: `Parametry: poziom: ${interactionData.lvl}, modyfikator: ${interactionData.okayModif}, dodatkowe AD: ${interactionData.addDmg}, krytyczne: ${interactionData.crit}, mnożnik: ${interactionData.critMulti}, drugi atak: ${interactionData.second}`,
+                flags: 0
+            })
+        }
+    },
+};
